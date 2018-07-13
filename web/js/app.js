d9ad1b0c6a90c9d0a2880d98e761591ea7e6ebda
wlopera <lopera.william@gmail.com>
2018-07-13 01:09:48 -0500

    Ajuste para marquee - robot -> pasar a cultivo

--- web/js/app.js
+++ web/js/app.js
@@ -2,7 +2,7 @@
     "use strict";
 
     angular.module('RoboticaApp',[])
-        .controller("RoboticaController", ["$scope", function($scope){
+        .controller("RoboticaController", ["$scope", "$interval", function($scope, $interval){
             // Maxima Produccion
             $scope.totalMax = 0;
             // Tiempo de iteracion
@@ -24,17 +24,26 @@
             // Registro actual
             $scope.robotica = {id: 0, demanda: 10, producir: 5, importar: 10, paso: 3};
 
+            // Cantidad de producto a cultivar
+            $scope.xCultivar = 0;
+
+            // Cantidad de producto en cultivo
+            $scope.cultivando = 0;
+
+            $scope.unidad = " Ton.";
+
             // Mostrar el formulario modal
             $scope.mostrarModal = function(){
-                // Incrementar la iteracion
-                $scope.robotica.id = $scope.registros.length + 1;
 
-                // Incrementar la producion segun el periodo de iteracion
-                if($scope.registros.length > $scope.periodo - 1) {
-                  $scope.robotica.producir = parseInt($scope.robotica.producir) + $scope.incremento;
-                }
+              // Incrementar la iteracion
+              $scope.robotica.id = $scope.registros.length + 1;
 
-                $('#registroModal').modal('show');
+              // Incrementar la producion segun el periodo de iteracion
+              if($scope.registros.length > $scope.periodo - 1) {
+                $scope.robotica.producir = parseInt($scope.robotica.producir) + $scope.incremento;
+              }
+
+              $('#registroModal').modal('show');
             }
 
             // Procesar entrada de datos del modal
@@ -49,6 +58,10 @@
                 importar: robotica.importar,
                 paso: robotica.paso
               }
+
+              // Productos a cultivar
+              $scope.xCultivar = parseInt(robotica.producir);
+
               // Agregar el nuevo registro a la vista
               $scope.registros.push(record);
 
@@ -57,6 +70,8 @@
 
               // Ocultar la modal
               $('#registroModal').modal('hide');
+              //lego("yellow");
+              cultivar();
             };
 
             // Realizar proceso de cosecha
@@ -66,10 +81,47 @@
                   value.paso = parseInt(value.paso) - 1;
                   if(value.paso === 0) {
                     $scope.totalAlmacen = parseInt($scope.totalAlmacen) + parseInt(value.producir);
+                    $scope.cultivando -= parseInt(value.producir);
                     value.paso = -1;
                   }
                 }
               });
+            };
+
+            function lego(color) {
+              angular.element('.square').css("background-color", color);
+
             }
-        }])
+
+            function cultivar() {
+                var intX = 250;
+                var objDiv = document.getElementById('progress');
+                objDiv.innerHTML = " ===> " + $scope.xCultivar + " ===> ";
+                objDiv.style.background = "red";
+                objDiv.style.color = "black";
+                objDiv.style.position = "absolute";
+                objDiv.style.width = "14%";
+                objDiv.style.height = "5%";
+                move();
+                function move() {
+                  var objDiv = document.getElementById('progress');
+                  if ( objDiv != null ) {
+                      objDiv.style.left = (intX += 3).toString() + 'px';
+                  } //if
+                  if ( intX < 650 ) {
+                      setTimeout(move,25);
+                  } else {
+                      objDiv.innerHTML = "";
+                      objDiv.style.background ="";
+                      $scope.cultivando +=  parseInt($scope.xCultivar);
+                      $scope.xCultivar = 0;
+                      $scope.$apply();
+                  }
+
+                  return true;
+                }
+              }
+
+        }]);
+
 })()
